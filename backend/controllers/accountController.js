ab310fb0ab7857c224b171ac706c536e6d9ebc99
LAPTOP-H4Q87OAP\admin <khoavida@gmail.com>
2022-02-08 10:32:42 +0700

    Modify account Controller

--- backend/controllers/accountController.js
+++ backend/controllers/accountController.js
@@ -6,14 +6,21 @@ const base64url = require('base64url')
 function login(req, res) {
   var Username = req.body.Username;
   var Password = req.body.Password;
-  AccountDAO.getAccounts(Username, Password, (Account) => {
-    //login by email/phone
-    if (Account == -1) res.send('Account not registed');
+  AccountDAO.getAccounts(Username, (Account) => {
     if (Account == false) res.send('login failure');
     else {
-      let payload = Account;
-      let token = jwt.sign(JSON.stringify(payload), "secret");
-      res.send(token);
+      let acccount = Account;
+      bcrypt.compare(Password, acccount.Password, function (err, result) {
+        // result == true
+        if (result == true) {
+          let accessToken = jwt.sign({ id: acccount.AccountId }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+          let refreshToken = jwt.sign({ id: acccount.AccountId }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '1d' });
+          res.send({
+            accessToken: accessToken,
+            refreshToken: refreshToken
+          });
+        } else res.send('login failure');
+      })
     }
   });
 }
