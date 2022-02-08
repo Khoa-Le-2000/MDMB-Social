e3d9546c6cab40ef60bdef2f97cfeeca11836235
LAPTOP-H4Q87OAP\admin <khoavida@gmail.com>
2022-02-08 11:15:26 +0700

    Add close connection

--- backend/models/data-access/accountDAO.js
+++ backend/models/data-access/accountDAO.js
@@ -6,10 +6,11 @@ function getAccounts(Username, Callback) {
   var con = connection.createConnection();
   con.connect(function (err) {
     if (err) throw err;
-    console.log("Connected!");
+    // console.log("Connected!");
     var sql = `SELECT * FROM Account Where (Email=? or Phone =?)`;
     con.query(sql, [Username, Username],
       function (err, result) {
+        connection.closeConnection(con);
         if (err) throw err;
         if (result.length > 0) {
           var acc = new Account.Account(result[0].AccountId, result[0].Password, result[0].Phone, result[0].Email, result[0].Name, result[0].Avatar, result[0].Birthday, result[0].Gender, result[0].CreatedDate);
