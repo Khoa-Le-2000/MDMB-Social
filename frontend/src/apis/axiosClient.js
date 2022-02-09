b2d538446b0ce935092ae78f4d6ac39587587eda
hungpurdie <nmhung2022@gmail.com>
2022-02-09 09:23:41 +0700

    simple post login

---
+++ frontend/src/apis/axiosClient.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import queryString from 'query-string';
+
+const axiosClient = axios.create({
+  baseURL: process.env.BASE_API_URL,
+  headers: {
+    'content-type': 'application/json',
+  },
+  paramsSerializer: (params) => queryString.stringify(params),
+});
+
+axiosClient.interceptors.request.use(
+  async (config) => {
+    return config;
+  },
+  (error) => {
+    throw error;
+  }
+);
+
+axiosClient.interceptors.response.use(
+  (response) => {
+    if (response && response.data) {
+      return response.data;
+    }
+    return response;
+  },
+  (error) => {
+    // Handle errors
+    throw error;
+  }
+);
+
+export default axiosClient;
