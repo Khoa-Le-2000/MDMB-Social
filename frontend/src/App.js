91acbbe8bcf08b2e988ceb8580b6e967b8b72c50
hungpurdie <nmhung2022@gmail.com>
2022-02-17 15:43:37 +0700

    turn off animation fade in carousel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Routes, Route } from 'react-router-dom';
-import './App.scss';
-import React from 'react';
 import RequireAuth from 'components/RequireAuth';
-import Dashboard from 'features/Dashboard/Dashboard';
 import NotFound from 'features/404/NotFound';
+import Dashboard from 'features/Dashboard/Dashboard';
 import Home from 'features/Home/Home';
 import Register from 'features/Register/Register';
+import React from 'react';
 import { useSelector } from 'react-redux';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { getRedirect } from 'redux/selectors/authSelector';
+import './App.scss';
 
 function App() {
-  const isRedirectRegister = useSelector(getRedirect).register;
   return (
     <>
       <Routes>
@@ -24,10 +23,7 @@ function App() {
             </RequireAuth>
           }
         />
-        <Route
-          path="/register/*"
-          element={<Register isRedirectRegister={isRedirectRegister} />}
-        />
+        <Route path="/register/*" element={<Register />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
