4b3b817e9881d120e35c8673a751f75fd4199b55
hungpurdie <nmhung2022@gmail.com>
2022-02-18 08:24:34 +0700

    fix: resolve conflict

--- frontend/src/features/Login/Login.jsx
+++ frontend/src/features/Login/Login.jsx
@@ -7,14 +7,14 @@ import Hero3 from 'assets/images/heros/hero3.svg';
 import FacebookIcon from 'assets/images/icons/facebook.svg';
 import GoogleIcon from 'assets/images/icons/google.svg';
 import { useViewport } from 'hooks';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { Button, Carousel, Col, Form, Row } from 'react-bootstrap';
 import FacebookLogin from 'react-facebook-login';
 import GoogleLogin from 'react-google-login';
 import ReCAPTCHA from 'react-google-recaptcha';
 import { useForm } from 'react-hook-form';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link, Navigate, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import {
   login,
   loginByGoogle,
@@ -22,7 +22,6 @@ import {
   verifyCaptcha,
 } from 'redux/actions/authAction';
 import {
-  getAuth,
   getCaptcha,
   getErrorCount,
   getErrorLogin,
@@ -69,8 +68,6 @@ function Login() {
   const countError = useSelector(getErrorCount);
   const messageErrorLogin = useSelector(getErrorMessageLogin);
   const hasError = useSelector(getErrorLogin);
-
-  const isRedirectRegister = useSelector(getRedirect)?.register;
 
   const [message, setMessage] = useState('');
 
