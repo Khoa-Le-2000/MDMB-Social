a53e5db2c3ff44ca1c18e923e7810ff0dbfde842
hungpurdie <nmhung2022@gmail.com>
2022-03-02 09:49:16 +0700

    chat test ok

--- frontend/src/features/Home/Home.jsx
+++ frontend/src/features/Home/Home.jsx
@@ -1,14 +1,14 @@
+import { logout, refreshToken } from 'app/actions/login';
+import { getAuth } from 'app/selectors/loginSelector';
 import Login from 'features/Login/Login';
 import MainLayout from 'layouts/MainLayout';
 import React from 'react';
-import { Button, Container, Row, Col, Form } from 'react-bootstrap';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { logout, refreshToken } from 'app/actions/login';
-import './home.scss';
-import { getAuth } from 'app/selectors/loginSelector';
 import io from 'socket.io-client';
-
+import './home.scss';
+let socket;
 function Home() {
   const auth = useSelector(getAuth);
   const [message, setMessage] = React.useState('');
@@ -16,34 +16,27 @@ function Home() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const socket = io.connect(process.env.REACT_APP_API_URL, {
-    transports: ['websocket'],
-    auth: { token: auth?.accessToken },
-    query: { accountId: auth?.accountId },
-  });
-
   React.useEffect(() => {
+    socket = io.connect(process.env.REACT_APP_API_URL, {
+      transports: ['websocket'],
+      auth: { token: auth?.accessToken },
+      query: { accountId: auth?.accountId },
+    });
     socket.on('chat message', (data) => {
       setChat([...chat, data]);
     });
-  });
+  }, [auth?.accessToken, auth?.refreshToken, chat, auth?.accountId]);
+  console.log('🚀 :: file: Home.jsx :: line 37 :: socket', socket);
 
   const handleMessageChange = (e) => {
     setMessage(e.target.value);
-    console.log(
-      '🚀 :: file: Home.jsx :: line 33 :: e.target.value',
-      e.target.value
-    );
-    socket.emit('typing', 4);
+    socket.emit('typing', '4');
   };
 
   const handleSendMessage = (e) => {
     e.preventDefault();
-    socket.emit('chat message', message, '4', (res) => {
-      console.log('🚀 :: file: Home.jsx :: line 53 :: res', res);
-    });
+    socket.emit('chat message', message, '4', (res) => {});
   };
-
   const onHandleRefreshToken = () => {
     dispatch(refreshToken(auth.refreshToken));
   };
