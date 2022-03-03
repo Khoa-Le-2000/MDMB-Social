75e3445df2b54ffbd497f081f38dc4d855fe9983
cuongtruong2015 <cuongtruong2015@gmail.com>
2022-03-03 13:27:35 +0700

    api chatlist

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -36,13 +36,13 @@ function getOlderMessage(req, res) {
     });
 }
 function getChatList(req, res) {
-    let AccountId = req.body.AccountId;
+    let AccountId = req.query.AccountId;
     chatDao.getAccountReceived(AccountId, (AccountReceived) => {
         if (!AccountReceived) return res.status(401).send({result: "No messenger found"})
         var List = []
         for (let i = 0; i < AccountReceived.length; i++) {
             chatDao.getChatList(AccountId, AccountReceived[i], (ChatList) => {
-                List.push(ChatList);
+                List.push(ChatList); 
                 if (AccountReceived.length == List.length) {
                     return res.status(200).send(List)
                 }
