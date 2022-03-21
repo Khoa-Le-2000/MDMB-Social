ca4b32a6202740fc252e4f32a8f2d6d0990e5f46
hungpurdie <nmhung2022@gmail.com>
2022-03-21 16:28:33 +0700

    fix: update conversation on new message

--- frontend/src/app/selectors/conversations.js
+++ frontend/src/app/selectors/conversations.js
@@ -1,8 +1,18 @@
 export const getConversations = (state) =>
   state?.conversations?.listConversation;
 
+export const getFilterName = (state) => state?.conversations?.filterBy;
 
-export const getLengthMessageUnread= (accountId) => (state) => {
+export const getConversationsByFilter = (state) => {
+  const filterBy = getFilterName(state);
+  const conversations = state?.conversations?.listConversation;
+  if (filterBy === 'all') {
+    return conversations;
+  }
+  return conversations.filter((conversation) => conversation.UnseenMessage > 0);
+};
+
+export const getLengthMessageUnread = (accountId) => (state) => {
   const listConversation = getConversations(state);
   let length = 0;
   listConversation.forEach((item) => {
--- frontend/src/features/ChatOverView/ChatWindow/WindowContent/WindowContent.jsx
+++ frontend/src/features/ChatOverView/ChatWindow/WindowContent/WindowContent.jsx
@@ -1,9 +1,12 @@
 import MessageEmpty from 'features/ChatOverView/ChatWindow/WindowContent/MessageEmpty/MessageEmpty';
 import Messages from 'features/ChatOverView/ChatWindow/WindowContent/Messages/Messages';
 import React from 'react';
-import { getListMessageLatest } from 'app/selectors/chat';
-import { useSelector } from 'react-redux';
+import { getFetchingMessage, getListMessageLatest } from 'app/selectors/chat';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
+import { getAuth } from 'app/selectors/login';
+import { getMessagesLatest } from 'app/actions/chat';
+import { useParams } from 'react-router-dom';
 
 const Wrapper = styled.div`
   padding: 0px 5px;
@@ -12,11 +15,14 @@ const Wrapper = styled.div`
 
 function WindowContent({ typing, onSeenMessage }) {
   const messagesLatest = useSelector(getListMessageLatest);
+  const isFetching = useSelector(getFetchingMessage);
 
   return (
     <Wrapper>
       {messagesLatest.length > 0 ? (
         <Messages typing={typing} onSeenMessage={onSeenMessage} />
+      ) : isFetching ? (
+        <div>Loading</div>
       ) : (
         <MessageEmpty />
       )}
