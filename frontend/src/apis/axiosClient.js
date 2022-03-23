51c4765c2b98bef700e3fe6ac6001d3f18511cb0
cuongtruong2015 <cuongtruong2015@gmail.com>
2022-03-23 19:44:33 +0700

    bug register

--- frontend/src/apis/axiosClient.js
+++ frontend/src/apis/axiosClient.js
@@ -25,7 +25,6 @@ export const interceptor = (store) => {
   );
   axiosClient.interceptors.response.use(
     (response) => {
-      console.log(response)
       if (response && response.data) {
         return response.data;
       }
